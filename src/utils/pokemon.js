2eeb32acdcf778f866f3f4f9e3d97486e3aeedcf
menma0214 <116738944+menma0214@users.noreply.github.com>
2025-03-23 06:46:39 +0900

    fix nextButton and error handling

--- src/utils/pokemon.js
+++ src/utils/pokemon.js
@@ -6,13 +6,23 @@ export const getAllPokemon = (url) => {
   });
 };
 
-export const getPokemon = (url) => {
-  return new Promise((resolve, reject) => {
-    fetch(url)
-    .then((res) => res.json())
-    .then((data) => {
-      // console.log(data);
-      resolve(data);
-    });
-  });
-};
+// JSONの読み込みエラーがpromiseで発生するので修正
+// export const getPokemon = (url) => {
+//   return new Promise((resolve, reject) => {
+//     fetch(url)
+//     .then((res) => res.json())
+//     .then((data) => {
+//       // console.log(data);
+//       resolve(data);
+//     });
+//   });
+// };
+
+export const getPokemon = async (url) => {
+  const res = await fetch(url);
+  if (!res.ok) {
+    throw new Error(`HTTPエラー! ステータス: ${res.status}`);
+  }
+  const data = await res.json();
+  return data;
+}
